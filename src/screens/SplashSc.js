9d9242cab40680a8730f5ec3b3a89b1b56f3f080
kwangmin123455 <abdillah.kuhaku@gmail.com>
2025-01-15 14:31:06 +0800

    initial program

---
+++ src/screens/SplashSc.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const SplashSc = () => {
+  return (
+    <View>
+      <Text>SplashSc</Text>
+    </View>
+  );
+};
+
+export default SplashSc;
+
+const styles = StyleSheet.create({});
