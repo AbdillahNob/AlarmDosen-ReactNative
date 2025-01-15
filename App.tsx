a43965838e187dba76585340440c1864dc4441cb
kwangmin123455 <abdillah.kuhaku@gmail.com>
2025-01-15 14:45:03 +0800

    Desain UI Splash1-Complete

--- App.tsx
+++ App.tsx
@@ -2,7 +2,9 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
+
 import SplashSc from './src/screens/SplashSc';
+import SplashSc2 from './src/screens/SplashSc2';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +15,7 @@ const App = () => {
         initialRouteName="Splash"
         screenOptions={{headerShown: false}}>
         <Stack.Screen name="Splash" component={SplashSc} />
+        <Stack.Screen name="Splash2" component={SplashSc2} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/SplashSc.js
+++ src/screens/SplashSc.js
@@ -1,14 +1,53 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {View, Text, Image, StyleSheet, Alert} from 'react-native';
+import React, {useEffect} from 'react';
+import {StatusBar} from 'react-native';
+import {StackActions} from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
+import {
+  widthPercentageToDP as w,
+  heightPercentageToDP as h,
+} from '../utils/responsive';
 
 const SplashSc = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.dispatch(StackActions.replace('Splash2'));
+    }, 2500);
+  });
+
   return (
-    <View>
-      <Text>SplashSc</Text>
+    <View style={style.container}>
+      <StatusBar backgroundColor={'#0F4473'} barStyle={'light-content'} />
+      <View style={{alignItems: 'center'}}>
+        <Image
+          source={require('../assets/images/logoAlarm.png')}
+          style={{width: w(30), height: h(15)}}
+          resizeMode="contain"
+        />
+        <Text
+          style={{
+            textAlign: 'center',
+            marginTop: h(0.8),
+            fontSize: w(8),
+            color: '#F0A7A7',
+          }}>
+          ALARM APP
+        </Text>
+      </View>
     </View>
   );
 };
 
 export default SplashSc;
 
-const styles = StyleSheet.create({});
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0F4473',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
